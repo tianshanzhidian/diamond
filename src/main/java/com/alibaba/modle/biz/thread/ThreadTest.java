0a60b221f95fc3c54e26ec536321ffd02fb7d3f8
luolei <luolei@luoleideMacBook-Pro.local>
2018-11-15 18:57:06 +0800

    modify

--- src/main/java/com/alibaba/modle/biz/thread/ThreadTest.java
+++ src/main/java/com/alibaba/modle/biz/thread/ThreadTest.java
@@ -3,15 +3,31 @@ package com.alibaba.modle.biz.thread;
 import com.alibaba.modle.biz.Person;
 
 public class ThreadTest {
+    final static long timeInterval = 1000;
     public static void main(String[] args) throws InterruptedException {
         Person p = new Person();
 //        p.setName("hello");
 //        p.setAge("11");
 //        LogFileClient.put(p);
 //        Thread.sleep(20000);
-        //异常
-        Long id =null;
-        p.setId(id);
+
+//        Long id =null;
+//        p.setId(id);
+        Runnable runnable = new Runnable() {
+            public void run() {
+                while(true){
+                    System.out.println("hello");
+                    try{
+                        Thread.sleep(timeInterval);
+                    }catch(Exception e){
+
+                    }
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
 
     }
 }
